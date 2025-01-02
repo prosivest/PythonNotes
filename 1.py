5e0f3c979b5a22d590be501f1f203687288616a9
Flyfish-workcomputer <prosivest@hotmail.com>
2025-01-02 16:41:14 +0800

    数据类型

--- 1.py
+++ 1.py
@@ -7,3 +7,27 @@ print(a)
 # 浮点型
 b = 3.1415926
 print(b)
+print(b + 1.0)
+
+# 字符串
+c = "yuhaomiao"
+print(c)
+
+c1 = 'I\'m "OK"!'
+print(c1)
+
+# 转义字符\可以转义很多字符，比如\n表示换行，\t表示制表符，字符\本身也要转义，所以\\表示的字符就是\。
+# r'' 表示 '' 内部的字符不转义。
+print("line 1 \nline 2")
+print("line 1 \n\tline 2")
+print(r"line 1 \n\tline 2")
+
+# 布尔型
+d = True
+print(d)
+print(3 > 1)
+
+# 空值
+e = None
+print(e)
+
