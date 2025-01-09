a232001c20e3cd710c625228082b2d9451bd6b82
Flyfish-workcomputer <prosivest@hotmail.com>
2025-01-09 16:54:14 +0800

    循环

---
+++ 07-循环.py
@@ -0,0 +1,11 @@
+j = 0
+for i in range(10):
+    j=j+i
+print(j)
+
+L = ['Bart', 'Lisa', 'Adam']
+for name in L:
+    print('hello,%s' %name)
+    print('hello,{0:s}' .format(name))
+
+# 在循环中，break语句可以提前退出循环。
